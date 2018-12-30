ff2e35f72c63583787def0fce9aa3b9aebc7b092
TimmPure <T.j.m.voskuil@gmail.com>
2018-12-30 23:16:37 +0100

    Agents spawn at random locations

--- Steering/Assets/Flock.cs
+++ Steering/Assets/Flock.cs
@@ -8,13 +8,17 @@ public class Flock : MonoBehaviour {
     public GameObject[] flock;
     public int agentCount = 100;
     private float leftConstraint, rightConstraint, topConstraint, bottomConstraint;
-   
-	void Start () {
+    private Camera cam;
+
+    void Start()
+    {
+        cam = Camera.main;
         flock = new GameObject[agentCount];
         
         for (int i = 0; i < flock.Length; i++)
         {
-            GameObject obj = Instantiate(agent, this.transform.position, Quaternion.identity, this.transform) as GameObject;
+
+            GameObject obj = Instantiate(agent, cam.ViewportToWorldPoint(new Vector3(Random.value,Random.value,10f)), Quaternion.identity, this.transform) as GameObject;
             obj.GetComponent<Rigidbody2D>().AddForce(Random.insideUnitCircle*1000f);
             flock[i] = obj;
         }
