ba7f1195b7d02c75bbf6d433c72c835ebbf9de94
TimmPure <T.j.m.voskuil@gmail.com>
2018-12-30 22:32:58 +0100

    Spawn flock of agents with random velocities

---
+++ Steering/Assets/Flock.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flock : MonoBehaviour {
+
+    public GameObject agent;
+    public GameObject[] flock;
+    public int agentCount = 100;
+
+	// Use this for initialization
+	void Start () {
+        flock = new GameObject[agentCount];
+
+        for (int i = 0; i < flock.Length; i++)
+        {
+            GameObject obj = Instantiate(agent, this.transform.position, Quaternion.identity) as GameObject;
+            obj.transform.parent = this.transform;
+            obj.GetComponent<Rigidbody2D>().AddForce(Random.insideUnitCircle*1000f);
+            flock[i] = obj;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
