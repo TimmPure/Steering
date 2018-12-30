491a45853e969021b2e4add82c9516f574331623
TimmPure <T.j.m.voskuil@gmail.com>
2018-12-30 23:01:37 +0100

    Agents Screenwrap

---
+++ Steering/Assets/Agent.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Agent : MonoBehaviour {
+
+    public float leftConstraint, rightConstraint, topConstraint, bottomConstraint;
+    private Camera cam;
+    
+    void Start () {
+        cam = Camera.main;
+        leftConstraint = cam.ScreenToWorldPoint(Vector3.zero).x;
+        rightConstraint = cam.ScreenToWorldPoint(new Vector3(Screen.width, 0f, 0f)).x;
+        topConstraint = cam.ScreenToWorldPoint(new Vector3(0f, Screen.height, 0f)).y;
+        bottomConstraint = cam.ScreenToWorldPoint(Vector3.zero).y;
+    }
+
+	void Update () {
+		if(transform.position.x < leftConstraint)
+        {
+            transform.position = new Vector2(rightConstraint - 0.1f, transform.position.y);
+        }
+
+        if (transform.position.x > rightConstraint)
+        {
+            transform.position = new Vector2(leftConstraint + 0.1f, transform.position.y);
+        }
+
+        if (transform.position.y < bottomConstraint)
+        {
+            transform.position = new Vector2(transform.position.x, topConstraint - 0.1f);
+        }
+
+        if (transform.position.y > topConstraint)
+        {
+            transform.position = new Vector2(transform.position.x, bottomConstraint + 0.1f);
+        }
+    }
+}
--- Steering/Assets/Flock.cs
+++ Steering/Assets/Flock.cs
@@ -7,21 +7,19 @@ public class Flock : MonoBehaviour {
     public GameObject agent;
     public GameObject[] flock;
     public int agentCount = 100;
-
-	// Use this for initialization
+    private float leftConstraint, rightConstraint, topConstraint, bottomConstraint;
+   
 	void Start () {
         flock = new GameObject[agentCount];
-
+        
         for (int i = 0; i < flock.Length; i++)
         {
-            GameObject obj = Instantiate(agent, this.transform.position, Quaternion.identity) as GameObject;
-            obj.transform.parent = this.transform;
+            GameObject obj = Instantiate(agent, this.transform.position, Quaternion.identity, this.transform) as GameObject;
             obj.GetComponent<Rigidbody2D>().AddForce(Random.insideUnitCircle*1000f);
             flock[i] = obj;
         }
 	}
 	
-	// Update is called once per frame
 	void Update () {
 		
 	}
